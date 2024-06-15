86fe31e4df4c62b2927459bb243bcd57e1d98900
Topi Järvinen <topi.jarvinen1@gmail.com>
2024-06-15 12:07:18 +0300

    testing0.5

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -15,11 +15,11 @@ export class Game extends Scene
         Animations.create(this)
         ObjectAnimations.create(this)
 
-        this.lights.enable()
-        this.lights.enable().setAmbientColor(0x070708);
+        //this.lights.enable()
+        //this.lights.enable().setAmbientColor(0x070708);
 
         this.priest = new PlayerControls(this, 120,300)
-        this.priest.setPipeline("Light2D")
+        //this.priest.setPipeline("Light2D")
 
         
 
@@ -29,7 +29,7 @@ export class Game extends Scene
 
         const ground = testMap.addTilesetImage("ground", "groundtiles", 16,16);
 
-        const firstLayer = testMap.createLayer("ground", ground).setPipeline("Light2D")
+        const firstLayer = testMap.createLayer("ground", ground)
         firstLayer.depth = 1;
 
         firstLayer.setCollisionByProperty({ collides: true });
@@ -47,27 +47,27 @@ export class Game extends Scene
         this.fire1 = this.add.sprite(58,50,"fire")
         this.fire1.depth = 2
         this.fire1.anims.play("firePlay")
-        this.light1 = this.lights.addLight(58,50,100)
+        //this.light1 = this.lights.addLight(58,50,100)
 
         this.fire2 = this.add.sprite(198,50,"fire")
         this.fire2.depth = 2
         this.fire2.anims.play("firePlay")
-        this.light2 = this.lights.addLight(198,50,100)
+        //this.light2 = this.lights.addLight(198,50,100)
 
         this.fire3 = this.add.sprite(58,205,"fire")
         this.fire3.depth = 2
         this.fire3.anims.play("firePlay")
-        this.light3 = this.lights.addLight(58,205,100)
+        //this.light3 = this.lights.addLight(58,205,100)
 
         this.fire4 = this.add.sprite(198,205,"fire")
         this.fire4.depth = 2
         this.fire4.anims.play("firePlay")
-        this.light4 = this.lights.addLight(198,205,100)
+        //this.light4 = this.lights.addLight(198,205,100)
 
-        this.campfire1 = this.add.sprite(70,320,"campfire").setPipeline("Light2D")
+        this.campfire1 = this.add.sprite(70,320,"campfire")
         this.campfire1.depth = 2
         this.campfire1.anims.play("campfire")
-        this.light5 = this.lights.addLight(70,320,100)
+        //this.light5 = this.lights.addLight(70,320,100)
 
        
     }
--- src/scenes/UI/IngameUI.js
+++ src/scenes/UI/IngameUI.js
@@ -8,10 +8,18 @@ export class IngameUI extends Scene {
     }
 
     preload() {
+        this.load.bitmapFont('baseFontUI', 'assets/fonts/baseFont_0.png', 'assets/fonts/baseFont.fnt')
+
         this.load.image("ingameBorders", "assets/ui/ingameBorders.png")
         this.load.image("mainUI", "assets/ui/mainUI.png")
+        
 
         this.load.spritesheet("inventoryBtn", "assets/ui/inventoryBtn.png", {frameWidth: 19, frameHeight: 18})
+        this.load.spritesheet("skillsBtn", "assets/ui/skillsBtn.png", {frameWidth: 19, frameHeight: 18})
+
+        this.load.spritesheet("priestPortrait", "assets/portraits/priestPortrait.png", {frameWidth: 44, frameHeight: 26})
+        this.load.spritesheet("magusPortrait", "assets/portraits/magusPortrait.png", {frameWidth: 44, frameHeight: 26})
+        this.load.spritesheet("warPortrait", "assets/portraits/warPortrait.png", {frameWidth: 44, frameHeight: 26})
     }
 
     create() {
@@ -37,7 +45,55 @@ export class IngameUI extends Scene {
         
         //this.inventoryText = this.add.bitmapText(525,47,"baseFontUI","Inventory",16).setVisible(false)
         
-       
+       //Skills Button
+       this.skillsButton = this.add.sprite(297, 139, "skillsBtn").setFrame(0)
+       .setInteractive({ useHandCursor: true })
+       .on('pointerdown', () => {
+           this.toggleInventory();
+       })
+       .on('pointerover', () => {this.skillsButton.setFrame(1)})
+       .on('pointerout', () => {this.skillsButton.setFrame(0)});
+       this.skillsButton.depth = 21
+
+
+
+       //PORTRAITS
+
+       //Priest Portrait
+       this.priestPortrait = this.add.sprite(285, 112, "priestPortrait").setFrame(0)
+       .setInteractive({ useHandCursor: true })
+       .on('pointerdown', () => {
+           this.toggleInventory();
+       })
+       .on('pointerover', () => {this.priestPortrait.setFrame(1)})
+       .on('pointerout', () => {this.priestPortrait.setFrame(0)});
+       this.priestPortrait.depth = 21
+
+       //this.priestName = this.add.bitmapText(280,112,"baseFontUI","Aserius",16)
+       //this.priestName.scale = 0.5
+       //this.priestName.depth = 22
+
+       //Magus Portrait
+       this.magusPortrait = this.add.sprite(285, 82, "magusPortrait").setFrame(2)
+       .setInteractive({ useHandCursor: true })
+       .on('pointerdown', () => {
+           this.toggleInventory();
+       })
+       .on('pointerover', () => {this.magusPortrait.setFrame(3)})
+       .on('pointerout', () => {this.magusPortrait.setFrame(2)});
+       this.magusPortrait.depth = 21
+
+       //War Portrait
+       this.warPortrait = this.add.sprite(285, 52, "warPortrait").setFrame(0)
+       .setInteractive({ useHandCursor: true })
+       .on('pointerdown', () => {
+           this.toggleInventory();
+       })
+       .on('pointerover', () => {this.warPortrait.setFrame(1)})
+       .on('pointerout', () => {this.warPortrait.setFrame(0)});
+       this.warPortrait.depth = 21
+
+
 
         
     }
